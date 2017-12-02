d02251cea92f496d480469aa98a852b4ccce0cbd
Luca Ceriani <luca.ceri@gmail.com>
2017-12-02 10:57:24 +0100

    bug fix

--- dashButtonBasic.py
+++ dashButtonBasic.py
@@ -1,20 +1,26 @@
+import time
 try:
 	from scapy.all import *
 except ImportError:
 	raise ImportError("You have to install scapy module.")
 
 class dashButton:
+		
 	def __init__(self, macAddress, execFunction):
 		self.__macAddress=macAddress
 		self.__execFunction=execFunction
+		self.__lastExecution=0
 
 	def startListening(self):
 		sniff(prn=self.__arp_display, filter="arp", store=0, count=0)
 
 	def __arp_display(self, pkt):
-		if pkt[ARP].op == 1:
-			if pkt[ARP].hwsrc == self.__macAddress:
+		if (pkt[ARP].op==1) and (pkt[ARP].hwsrc == self.__macAddress):
+			# controllo per non eseguire una funzione 4/5 volte di seguito
+			if int(time.time())-self.__lastExecution > 2:
+				# se sono passati almeno due secondi dall'ultima esecuzione
 				self.__execFunction(self)
+				self.__lastExecution=int(time.time())
 	
 	def getMacAddress(self):
 		return self.__macAddress
